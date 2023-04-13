e8c0c96b0b906d571b06f8cab5db00a63f57ea18
Konrad Sosnowski <sosnowski.konrad@gmail.com>
2023-04-14 09:47:16 +1000

    adding more known errors

--- src/utils.ts
+++ src/utils.ts
@@ -68,7 +68,7 @@ export function aggregateTrades(trades: Trade[]): Trade {
 
 export function translateError(error): any {
   const asStr = `${error}`;
-  const knownErrors = ["Transaction has been reverted by the EVM"];
+  const knownErrors = [`Transaction has been reverted by the EVM`, `CONNECTION ERROR: Couldn't connect to node on WS`];
   for (var knownError of knownErrors) {
     if (asStr.includes(knownError)) return knownError;
   }
